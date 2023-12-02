a15260b54fc854ec175539497b8fbb71ce1f2f19
Navashub <navasmuller01@gmail.com>
2023-12-02 12:00:31 +0300

    completed assignments

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,13 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+        
+    def __init__(self, first_name, last_name, knowlegde=[]):
+            super().__init__(first_name, last_name)
+            self.knowledge = knowlegde
+
+    def learn(self,unit):
+        self.unit=unit
+        self.knowledge.append(unit)    
+            
+        
